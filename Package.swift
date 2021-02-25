bc0e070f84f99361a0cec1f2780382de8255befa
Andy Liu <andy@madmachine.io>
2021-02-25 21:48:29 +0800

    Test
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch("main"))
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", .branch("release/v0.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
