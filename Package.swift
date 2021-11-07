2bcaccd312e3cc56b48a9f34f761b15695c9d2d9
Andy Liu <andy@madmachine.io>
2021-11-07 23:02:05 +0800

    Add buffer write function for ST7789 (#7)
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", .upToNextMajor(from: "0.0.4")),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", .upToNextMajor(from: "0.0.5")),
         .package(url: "https://github.com/madmachineio/MadDisplay.git", .upToNextMajor(from: "0.0.1"))
     ],
     targets: [
