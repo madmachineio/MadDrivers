328ef02a4f11e3e649bfd9f6a8ae8a8264b4b288
Ines Zhou <72301128+Ines333@users.noreply.github.com>
2021-11-15 18:12:40 +0800

    add ds3231 (#8)
    * add ds3231
    
    * change dependencies

---
+++ Examples/DS3231/ReadTime/Sources/ReadTime/main.swift
@@ -0,0 +1,25 @@
+import SwiftIO
+import MadBoard
+import DS3231
+
+let i2c = I2C(Id.I2C0)
+let sensor = DS3231(i2c)
+
+// If the RTC has stopped work due to power off, you need to reset the time. 
+// If not, the time will not be reset. 
+// Of course, you need to change the time below to your time.
+let current = DS3231.Time(
+    year: 2021, month: 11, day: 12, hour: 16,
+    minute: 37, second: 42, dayOfWeek: 5)
+sensor.setTime(current)
+
+
+while true {
+    // Read current time. It returns a optional value and you need to unwrap it.
+    let time = sensor.readCurrent()
+    if let time = time {
+        print(time)
+    }
+    
+    sleep(ms: 2000)
+}
