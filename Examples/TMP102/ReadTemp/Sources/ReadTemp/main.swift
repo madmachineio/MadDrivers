6d1901b081168c9be999304bf7a9828b9efb9e5d
Andy Liu <andy@madmachine.io>
2024-09-21 17:35:18 +0700

    Update TMP102
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/TMP102/ReadTemp/Sources/ReadTemp/main.swift
+++ Examples/TMP102/ReadTemp/Sources/ReadTemp/main.swift
@@ -9,5 +9,11 @@ print("Start read tempatur with aw TMP102 sensor.")
 
 while (true) {
     sleep(ms: 2000)
-    print("Tempature is \(sensor.readCelcius()) C.")
+    print("Tempature is \(getDoubleString(sensor.readCelcius())) C.")
+}
+
+func getDoubleString(_ num: Double) -> String {
+    let int = Int(num)
+    let frac = Int((num - Double(int)) * 100)
+    return "\(int).\(frac)"
 }
