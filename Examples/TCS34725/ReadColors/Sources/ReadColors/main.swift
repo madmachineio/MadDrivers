50a063b310602bc9c999fc0ede80c77e1a989990
Andy Liu <andy@madmachine.io>
2024-09-23 16:57:56 +0700

    Update TCS34725
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/TCS34725/ReadColors/Sources/ReadColors/main.swift
+++ Examples/TCS34725/ReadColors/Sources/ReadColors/main.swift
@@ -9,9 +9,17 @@ let sensor = TCS34725(i2c)
 sensor.setIntegrationTime(150)
 
 while true {
-    print(sensor.readRaw())
-    print("Lux: \(sensor.readLux())")
-    print("Color temperature: \(sensor.readColorTemperature())")
-    print("Color code: \(String(sensor.readColorCode(), radix: 16))")
+    let rawValue = sensor.readRaw()
+    print("Rawvalue: red = \(rawValue.red), green = \(rawValue.green), blue = \(rawValue.blue), clear = \(rawValue.clear)")
+    print("Lux: \(getFloatString(sensor.readLux()))")
+    print("Color temperature: \(getFloatString(sensor.readColorTemperature()))")
+    print("Color code: \(sensor.readColorCode()), radix: 16")
     sleep(ms: 1000)
+}
+
+
+func getFloatString(_ num: Float) -> String {
+    let int = Int(num)
+    let frac = Int((num - Float(int)) * 100)
+    return "\(int).\(frac)"
 }
