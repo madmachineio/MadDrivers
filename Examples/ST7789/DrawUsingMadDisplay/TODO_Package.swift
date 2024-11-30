ae33d22e54140c53c4e523b0fefa9d505461321f
Andy Liu <andy@madmachine.io>
2024-11-30 17:41:47 +0700

    Fully compatible with the official Swift toolchain
    Signed-off-by: Andy Liu <andy@madmachine.io>

--- Examples/ST7789/DrawUsingMadDisplay/TODO_Package.swift
+++ Examples/ST7789/DrawUsingMadDisplay/TODO_Package.swift
@@ -5,10 +5,10 @@ let package = Package(
     name: "DrawUsingMadDisplay",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "develop"),
-        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "develop"),
-        .package(url: "https://github.com/madmachineio/MadDrivers.git", branch: "develop"),
-        .package(url: "https://github.com/madmachineio/MadDisplay.git", branch: "develop"),
+        .package(url: "https://github.com/madmachineio/SwiftIO.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadBoards.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadDrivers.git", branch: "main"),
+        .package(url: "https://github.com/madmachineio/MadDisplay.git", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
